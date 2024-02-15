1b52674137421f7d4a8c015a5a348322d44a3553
chaewon-moana <dreamytaurus508@gmail.com>
2024-02-15 09:32:09 +0900

    tableCell 기능 구현

---
+++ SeSACReminders/DateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DateViewController.swift
+//  SeSACReminders
+//
+//  Created by cho on 2/14/24.
+//
+
+import UIKit
+import SnapKit
+
+class DateViewController: BaseViewController {
+    
+    let datePicker = UIDatePicker()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    override func setAddView() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+    
+    override func configureAttribute() {
+        
+    }
+}
